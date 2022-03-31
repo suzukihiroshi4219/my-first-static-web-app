07921190ade70d647f2d7598fa85ac5f451ddb0a
suzukihiroshi4219 <suzuhiro4219@gmail.com>
2022-03-31 22:12:35 +0900

    pusutest

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
+import push from 'push.js';
 
 @Component({
   selector: 'app-root',
   template: `<div>Hello {{value}}</div>`,
 })
 export class AppComponent {
-  value = 'World2';
+  value = 'Welcome to Suzuki site';
+
+  ngOnInit() {
+    push.create('I am Suzuki Yeah!');
+  }
 }
